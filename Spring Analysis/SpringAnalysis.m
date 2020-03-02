d255edbc02fb13b2683bf2f67000fa3c03fdcd31
divinesnowcones1 <61036005+divinesnowcones1@users.noreply.github.com>
2020-03-02 16:47:51 -0500

    Add files via upload
    Commented and added plots

--- Spring Analysis/SpringAnalysis.m
+++ Spring Analysis/SpringAnalysis.m
@@ -1,11 +1,11 @@
 % Constants
-m = 1;
+m = 0.5;
 g = 9.81;
 lb2N = 0.112985;
 in2m = 0.0254;
 %Requirements
 launchTraj = deg2rad(45);
-HorzDist = 0.3048;
+HorzDist = 0.1524;
 
 %Launch Velocity
 vf = sqrt((HorzDist*g)/sin(2*launchTraj));
@@ -19,7 +19,7 @@ Fx = F*cos(launchTraj);
 Freq = sqrt(Fy.^2 + Fx.^2);
 ThrustAngles = atan2(Fy,Fx);
 
-%Spring Analysis8.06
+%Range of link lengths
 linkLength = linspace(0.03,0.20,340); %cm
 
 
@@ -41,6 +41,7 @@ SpringMatrix = [3*in2m, 5.94*in2m, 2.2*lb2N;... %9044K193
     4.5*in2m, 8.34*in2m, 0.75*lb2N;...%9654K256
     5*in2m, 7.91*in2m, 0.82*lb2N;...  %9654K84
     5*in2m, 11.022*in2m, 2*lb2N;...   %9654K325
+    5*in2m, 11.2*in2m, 7.5*lb2N;...   %9654K738
     5.5*in2m, 8.5*in2m, 2.73*lb2N;... %9654K533
     5.5*in2m, 7.5*in2m, 3.09*lb2N;... %9654K471
     6*in2m, 10.65*in2m, 0.88*lb2N;... %9654K93
@@ -50,28 +51,32 @@ SpringMatrix = [3*in2m, 5.94*in2m, 2.2*lb2N;... %9044K193
     7.5*in2m, 15.2*in2m, 1.42*lb2N;...%9654K253
     8*in2m, 13.68*in2m, 1.26*lb2N;... %9654K145
     8*in2m, 12.95*in2m, 2.36*lb2N;... %9654K449
-    8*in2m, 12.9*in2m, 3.07*lb2N;...  %9654K444
-    5*in2m, 7.2*in2m, 30*lb2N];
+    8*in2m, 12.9*in2m, 3.07*lb2N];...  %9654K444
+%     5*in2m, 7.2*in2m, 30*lb2N];
 
+%Spring constant
+k = SpringMatrix(:,3);
+
+%Each spring displacment range
 xi = SpringMatrix(:,1);
 xf = SpringMatrix(:,2);
-k = SpringMatrix(:,3);
 x = zeros(size(SpringMatrix,1),100);
-
 for m = 1:size(SpringMatrix,1)
     x(m,:) = linspace(xi(m),xf(m),100);
 end
 
+%Variable declarations
 inc = 1;
 y = zeros(size(linkLength,2),size(x,2),size(SpringMatrix,1));
 Fy = zeros(size(linkLength,2),size(x,2),size(SpringMatrix,1));
 torque = zeros(size(linkLength,2),size(x,2),size(SpringMatrix,1));
 angle = zeros(size(linkLength,2),size(x,2),size(SpringMatrix,1));
 
+%Spring Calculations
 for i = 1:size(SpringMatrix,1)
     for j = 1:size(linkLength,2)
         for l = 1:size(x,2)
-            if linkLength(j)*2 > xf(i) 
+            if linkLength(j)*2 > xf(i)
                 y(j,l,i) = sqrt(linkLength(j)^2 - (x(i,l)/2)^2) + sqrt(linkLength(j)^2 - (x(i,l)/2)^2);
                 Fy(j,l,i) = k(i)*y(j,l,i)*(1 - xi(i)/sqrt(4*linkLength(j)^2 - y(j,l,i)^2));
                 torque(j,l,i) = (x(i,l) - xi(i))*k(i)*sqrt(linkLength(j)^2 - (x(i,l)/2)^2);
@@ -84,14 +89,15 @@ for i = 1:size(SpringMatrix,1)
     end
 end
 
+%Average spring force
 Fyavg = mean(Fy,2);
-% [row,col,v] = ind2sub(size(Fyavg), find(Fyavg >= F));
+
 inc = 1;
 Fybest = 0;
 for i = 1:size(Fyavg,3)
     for j = 1:size(linkLength,2)
         temp = Fyavg(j,1,i);
-        if Fyavg(j,1,i) > Freq
+        if Fyavg(j,1,i) > min(Freq)
             row(inc) = j;
             col(inc) = i;
             inc = inc  +  1;
@@ -104,6 +110,14 @@ for i = 1:size(Fyavg,3)
     end
 end
 
-SpringMatrix(springs(spring),:)
-linkLength(links(link))
+%Best spring 
+disp(['Spring Number: ',num2str(spring),'']);
+SpringMatrix(spring,:)
+linkLength(link)
 Fybest
+figure(2);
+plot(rad2deg(angle(link,:,spring)),torque(link,:,spring));
+title('Spring/Link angle vs torque');
+figure(3);
+plot(x(spring,:),Fy(link,:,spring));
+title('Distance vs Fy');
