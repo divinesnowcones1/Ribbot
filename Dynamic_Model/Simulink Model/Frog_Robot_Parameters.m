8e39b2767977a1d35dfad6086bff02ddf8da34fc
alliWong <60459559+alliWong@users.noreply.github.com>
2020-04-26 05:40:29 -0400

    Update Frog_Robot_Parameters.m

--- Dynamic_Model/Simulink Model/Frog_Robot_Parameters.m
+++ Dynamic_Model/Simulink Model/Frog_Robot_Parameters.m
@@ -1,46 +1,42 @@
-close all;
-clc;
-
-%% Robot Parameters
-mass = 1; % kg 0.50592
-g = 9.81;
-
 %% Leg parameters
-% Need to change
 %Foot Parameters
 footLength = 8;     % cm
 footWidth = 3;      % cm
 footHeight = 0.5;   % cm
+footMass = 0.06;
 
 %Back Leg Parameters (cm)
 rUpperLegLength = 0.63;
 rUpperLegWidth = 1.91;
 rUpperLegHeight = 11.45;
 rUpperLegDim = [rUpperLegLength, rUpperLegWidth, rUpperLegHeight];
+rUpperLegMass = 0.1;
+
 rLowerLegLength = 0.63;
 rLowerLegWidth = 1.91;
 rLowerLegHeight = 11.45;
 rLowerLegDim = [rLowerLegLength, rLowerLegWidth, rLowerLegHeight];
+rLowerLegMass = 0.1;
 
 %Front Leg Parameters (cm)
 fLegLength = 0.32;
 fLegWidth = 1.27;
 fLegHeight = 7.06*2;
 fLegDim = [fLegLength, fLegWidth, fLegHeight];
-
-% fLegRadius = 0.5;
-% fLegLength = 15;
+fLegMass = 0.00188;
 
 %% Body Parameters
 %Rear Body Parameters
 rBodyLength = 12.70;    % cm
 rBodyWidth = 9;         % cm
 rBodyHeight = 9;        % cm
+rBodyMass = 0.19;
 
 %Front Body Parameters
 fBodyLength = 12.70;    % cm
 fBodyWidth = 9;         % cm
 fBodyHeight = 9;        % cm
+fBodyMass = 0.12;
 
 %% World Parameters
 height_plane = 0.025;
@@ -53,6 +49,9 @@ world_damping = 0;      % Translational damping for 6-DOF joint [N/m]
 world_rot_damping = 0;  % Rotational damping for 6-DOF joint [N*m/(rad/s)]
 
 %% Motion Parameters
+mass = footMass + rUpperLegMass + rLowerLegMass + fLegMass + rBodyMass + fBodyMass; % kg 0.50592
+g = 9.81;
+
 motion_time_constant = 0.001;
 
 % Contact and friction parameters
